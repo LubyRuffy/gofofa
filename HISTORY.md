b09a7046728ab845888c90eb19c5ee5a94bad7ab
lubyruffy <lubyruffy@gmail.com>
2022-05-17 11:15:14 +0800

    add subcommands, update goreleaser.yaml

--- HISTORY.md
+++ HISTORY.md
@@ -1,3 +1,3 @@
-## v0.0.1
+## v0.0.1 initial release
 - add search/account subcommand
 - add csv/json/xml output format
---
+++ scripts/tag_release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# 从HISTORY.md中进行版本提取
+if [[ ! -f HISTORY.md ]]; then
+  echo "cannot found HISTORY.md file"
+  exit
+fi
+
+VERSION=`head -n1 HISTORY.md | awk '{print $2}' | tr " " _`
+if [[ ! $VERSION =~ ^v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+$ ]]; then
+  echo "version format error: $VERSION, HISTORY.md first line must be '## v0.0.1 <commentstring>' format "
+  exit
+fi
+
+COMMENT=`head -n1 HISTORY.md | awk '{$1="";$2="";sub("  "," ");print $0;}'`
+if [[ $COMMENT == "" || $COMMENT == " " ]]; then
+  echo "no comment: HISTORY.md first line must be '## v0.0.1 <commentstring>' format "
+  exit
+fi
+
+git tag -a $VERSION -m "$COMMENT"
+git push origin $VERSION
+goreleaser release --rm-dist --debug
